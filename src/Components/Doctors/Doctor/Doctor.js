2c8f92d0c4c2028b95c408e3a7da8eb12b8223fc
Mahir-Ahmed-250 <xahin.mahir@gmail.com>
2021-10-20 16:18:04 +0600

    Added Some Style

--- src/Components/Doctors/Doctor/Doctor.js
+++ src/Components/Doctors/Doctor/Doctor.js
@@ -4,7 +4,7 @@ import { Card } from 'react-bootstrap';
 const Doctor = ({ doctor }) => {
     const { name, img } = doctor
     return (
-        <div className="col-md-6 col-lg-3 mt-5 ">
+        <div className="col-md-6 col-lg-3 mt-5 animate__animated animate__fadeInLeft ">
             <div  >
                 <Card className='card' >
                     <Card.Img variant="top" src={img} />
